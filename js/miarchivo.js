45516816168f53ad13ac95b6df793c6615e41335
franHellbusch <fghellbusch@gmail.com>
2022-05-06 16:27:37 -0300

    agregando carousel

---
+++ js/miarchivo.js
@@ -0,0 +1,9 @@
+class Producto{
+    constructor (imagen, nombre, desc, precio){
+        this.imagen = imagen
+        this.nombre = nombre
+        this.desc = desc
+        this.precio = precio
+        this.vendido = false
+    }
+}
