a07a6d34175ebd88e2d7984ec9b0ffcf99dd8aa9
franHellbusch <fghellbusch@gmail.com>
2022-05-09 18:19:14 -0300

    carousel terminado

--- js/miarchivo.js
+++ js/miarchivo.js
@@ -18,7 +18,7 @@ let pantalon1 = new Producto ("pantalon-corto.jpg", "Pantalon corto", "Lorem ips
 let pantalon2 = new Producto ("pantalon-jean.webp", "Pantalon jean", "Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit culpa facere quisquam minima", "$3000");
 let pantalon3 = new Producto ("pantalon-jogging.webp", "Pantalon jogging", "Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit culpa facere quisquam minima", "$2800");
 // buzo
-let buzo1 = new Producto ("buzo-capucha.webp", "Buzo con capucha", "Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit culpa facere quisquam minima", "$4000");
+let buzo1 = new Producto ("buzo-capucha.jpg", "Buzo con capucha", "Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit culpa facere quisquam minima", "$4000");
 let buzo2 = new Producto ("buzo-sin-capucha.png", "Buzo sin capucha", "Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit culpa facere quisquam minima", "$3500");
 
 const remeras = {remera1, remera2, remera3};
@@ -48,9 +48,9 @@ const productos = [remeras, pantalones, buzos];
 //     }
 // }
 
-function card(imagen, nombre, desc, precio){
-    alert(`CARD ${numero}\nLa imagen es: ${imagen}\nEl nombre es: ${nombre}\nLa desc es: ${desc}\nEl precio es: ${precio}`)
-}
+// function card(imagen, nombre, desc, precio){
+//     alert(`CARD ${numero}\nLa imagen es: ${imagen}\nEl nombre es: ${nombre}\nLa desc es: ${desc}\nEl precio es: ${precio}`)
+// }
 
 // let num = parseInt(prompt("numero"));
 // for (let propiedad in productos[num]){
@@ -66,8 +66,6 @@ function card(imagen, nombre, desc, precio){
 //     numero += 1
 // }
 
-let cardDeck = document.getElementById("card-deck");
-
 // let cards = document.createElement("div");
 // cards.className = "col mb-4";
 
@@ -75,12 +73,62 @@ let cardDeck = document.getElementById("card-deck");
 
 // cardDeck.append(cards)
 
-let num = parseInt(prompt("numero"))
-for (let propiedad in productos[num]){
-    for (i = 0;i <1; i++){
-        let cards = document.createElement("div");
-        cards.className = "col mb-4";
-        cards.innerHTML = `<div id="card" class="card${i + 1}"><img id="img" src="assets/img/${productos[num][propiedad].imagen}" class="card-img-top" alt="paisaje"><div id="card-body" class="card-body"><h5 class="card-title">${productos[num][propiedad].nombre}</h5><p class="card-text">${productos[num][propiedad].desc}</p><a href="#" class="btn btn-primary">${productos[num][propiedad].precio}</a></div></div>`;
-        cardDeck.append(cards)
+let cardDeck = document.getElementById("card-deck");
+
+function filtros() {
+    for (let propiedad in productos[num]){
+        for (i = 0;i <1; i++){
+            let cards = document.createElement("div");
+            cards.className = "col mb-4";
+            cards.innerHTML = `<div id="card" class="card${i + 1}"><img id="img" src="assets/img/${productos[num][propiedad].imagen}" class="card-img-top" alt="paisaje"><div id="card-body" class="card-body"><h5 class="card-title">${productos[num][propiedad].nombre}</h5><p class="card-text">${productos[num][propiedad].desc}</p><a href="#" class="btn btn-primary">${productos[num][propiedad].precio}</a></div></div>`;
+            cardDeck.append(cards)
+        }
+    }
+}
+
+let num = 0
+filtros()
+let botonRemeras = document.getElementById("remeras");
+let botonPantalones = document.getElementById("pantalones");
+let botonBuzos = document.getElementById("buzos");
+let botonTodos = document.getElementById("todos");
+
+botonRemeras.onclick = () => {
+    while (cardDeck.firstChild) {
+        cardDeck.removeChild(cardDeck.firstChild);
+    }
+    num = 0;
+    filtros()
+}
+
+botonPantalones.onclick = () => {
+    while (cardDeck.firstChild) {
+        cardDeck.removeChild(cardDeck.firstChild);
+    }
+    num = 1;
+    filtros()
+}
+
+botonBuzos.onclick = () => {
+    while (cardDeck.firstChild) {
+        cardDeck.removeChild(cardDeck.firstChild);
+    }
+    num = 2;
+    filtros()
+}
+
+botonTodos.onclick = () => {
+    while (cardDeck.firstChild) {
+        cardDeck.removeChild(cardDeck.firstChild);
+    }
+    for (i = 0; i < 3; i++){
+        for (let propiedad in productos[i]){
+            for (index = 0;index <1; index++){
+                let cards = document.createElement("div");
+                cards.className = "col mb-4";
+                cards.innerHTML = `<div id="card" class="card${i + 1}"><img id="img" src="assets/img/${productos[i][propiedad].imagen}" class="card-img-top" alt="paisaje"><div id="card-body" class="card-body"><h5 class="card-title">${productos[i][propiedad].nombre}</h5><p class="card-text">${productos[i][propiedad].desc}</p><a href="#" class="btn btn-primary">${productos[i][propiedad].precio}</a></div></div>`;
+                cardDeck.append(cards)
+            }
+        }
     }
 }
